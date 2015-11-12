1168489635b7a6054cfb56d3b588b221bc83df74
Emmanuel Petit <epetit11@gmail.com>
2015-11-12 23:31:13 +0100

    Add browser data parse + db storage

--- index.js
+++ index.js
@@ -3,101 +3,143 @@ var express = require("express")
 var app = express()
 var port = process.env.PORT || 5000
 var server = http.createServer(app)
-
 var CryptoJS = require("crypto-js")
+var bodyParser = require('body-parser')
 
-var visiteurs = {};
 
-var Logger = function(level){
-	
-	this.levels = ['DEBUG','INFO','WARNING','ERROR']
-	
-	
-	this.log = function(l, data){
-		
-		if( data instanceof Object){
+var loki = require('lokijs')
+var db = new loki('database/browsers.json')
+var visiteurs = null
+
+db.loadDatabase({}, function () {
+   visiteurs = db.getCollection('users')
+  	if(!visiteurs){
+  		visiteurs = db.addCollection('users')
+  	}
+});
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+	extended: false
+}))
+
+// parse application/json
+app.use(bodyParser.json()) // to support JSON-encoded bodies
+
+var Logger = function(level) {
+
+	this.levels = ['DEBUG', 'INFO', 'WARNING', 'ERROR']
+
+
+	this.log = function(l, data) {
+
+		if (data instanceof Object) {
 			data = JSON.stringify(data)
 		}
-		
-		console.log("["+l+"] " + data)
+
+		console.log("[" + l + "] " + data)
 	}
-	
-	this.d = function(data){
-		if(this.level == 0){
+
+	this.d = function(data) {
+		if (this.level == 0) {
 			this.log('DEBUG', data);
 		}
 	}
-	
-	this.i = function(data){
-		if(this.level <= 1){
+
+	this.i = function(data) {
+		if (this.level <= 1) {
 			this.log('INFO', data);
 		}
 	}
-	
-	this.w = function(data){
-		if(this.level <= 2 ){
+
+	this.w = function(data) {
+		if (this.level <= 2) {
 			this.log('WARNING', data);
 		}
 	}
-	
-	this.e = function(data){
-		if(this.level <= 3){
+
+	this.e = function(data) {
+		if (this.level <= 3) {
 			this.log('ERROR', data);
 		}
 	}
-	
+
 	//init
-	
-	if(this.levels.indexOf(level) == -1){
+
+	if (this.levels.indexOf(level) == -1) {
 		this.level = 0
-	}else{
+	} else {
 		this.level = this.levels.indexOf(level)
 	}
-	
-	this.log('INFO', "Log level set to "+ this.levels[this.level])
-	
+
+	this.log('INFO', "Log level set to " + this.levels[this.level])
+
 }
 
 var log = new Logger('DEBUG')
 
-app.get('/', function (req, res) {
+app.get('/all', function(req, res) {
+	var data = visiteurs.find({})
+	res.json(data)
+})
+
+app.post('/unique', function(req, res) {
 
 	var headers = req.headers
 
-	var result = ""
+	var data = req.body.fingerprint
 
-	delete headers['if-none-match']
+	var valeur = data
 
-	var valeur = JSON.stringify(req.headers)
-  	var cle = CryptoJS.SHA256(valeur).toString()
+	var cle = CryptoJS.SHA256(valeur).toString()
 
-  	if(visiteurs[cle]){
-  		result = 'Déjà venu !<br>Info : '+cle+' ('+(Buffer.byteLength(cle, 'utf8'))+' bits) : '+valeur+' ('+(Buffer.byteLength(valeur, 'utf8'))+' bits)'
-  	}else{
-  		visiteurs[cle] = valeur
-  		result = 'Nouveau visiteur !<br>Info : '+cle+' : '+valeur
-  	}
-  	
-	res.send(req)
+	var result = {
+		cle: {
+			data: cle,
+			size: Buffer.byteLength(cle, 'utf8')
+		},
+		valeur: {
+			data: valeur,
+			size: Buffer.byteLength(valeur, 'utf8')
+		}
+	}
+
+	var existe = visiteurs.find({
+		'cle': cle
+	})
+
+	if (existe.length > 0) {
+		//result = 'Déjà venu !<br>Info : '+cle+' ('+(Buffer.byteLength(cle, 'utf8'))+' bits) : '+valeur+' ('+(Buffer.byteLength(valeur, 'utf8'))+' bits)'
+		console.log('Deja venu')
+		result.unique = false
+	} else {
+		visiteurs.insert({cle: cle,valeur: JSON.parse(valeur)})
+		db.saveDatabase()
+		//result = 'Nouveau visiteur !<br>Info : '+cle+' : '+valeur
+		console.log('Nouveau visiteur');
+		result.unique = true
+	}
+
+	res.json(result)
 });
 
 var options = {
-root: __dirname + '/public/',
-dotfiles: 'deny',
-headers: {
-    'x-timestamp': Date.now(),
-    'x-sent': true
-}
+	root: __dirname + '/public/',
+	dotfiles: 'deny',
+	headers: {
+		'x-timestamp': Date.now(),
+		'x-sent': true
+	}
 };
 
-app.get('/test', function(req, res){
+app.get('/', function(req, res) {
 
-	res.sendFile('index.html', options, function (err) {
-	    if (err) {
-	      console.log(err);
-	      res.status(err.status).end();
-	    }
-  	});
+	res.sendFile('index.html', options, function(err) {
+		if (err) {
+			console.log(err);
+			res.status(err.status).end();
+		}
+	});
 })
 
 //serve static files
---
+++ public/js/index.js
@@ -0,0 +1,94 @@
+$(document).ready(function() {
+
+    var formHeight = $('.welcome').outerHeight();
+    var welcomeHeight = $('.welcome').height();
+    // Create function to have matched heights
+    function getFormHeight() {
+        $('.welcome').css("height", welcomeHeight);
+        $('#form').css("min-height", formHeight);
+    }
+
+    function changeWelcomeHeight() {
+        var newWelcomeHeight = $('#form').height();
+    }
+
+    // Run our function to get heights
+    getFormHeight();
+
+    // Fade and Show elements when 'share your ideas' button clicked
+    $('.show').on('click', function() {
+        $('.fade').fadeOut();
+        $('#form').delay(300).fadeIn();
+    });
+
+    // Fade and Show elements when 'close' button clicked
+    $('.close').on('click', function() {
+        $('#form').fadeOut();
+        $('.fade').fadeIn();
+    });
+
+    $(window).resize(function() {
+        if ($('#form').css('display') != 'none') {
+            changeWelcomeHeight();
+        }
+    });
+
+    $("#verify").on('click', function() {
+
+        $("#debug").append("<p>Connexion au serveur ...</p>")
+        var browserdata = getBrowserData()
+
+        $.post('/unique', { fingerprint : browserdata}, function(reponse) {
+            $("#debug").append("<p>Réponse du serveur</p>")
+
+            if(reponse.cle){
+                $("#debug").append('<p>Clé : ' + reponse.cle.data + ' ('+reponse.cle.size+' bits)</p>')   
+            }
+            
+            if(reponse.valeur){
+                $("#debug").append('<p>Valeur : ' + (reponse.valeur.data).substring(0,100)+'...' + ' ('+reponse.valeur.size+' bits)</p>')   
+            }
+
+            if(reponse.unique){
+                $("#result").css({'color':'green'})
+                $("#result").html('Vous n\'êtes jamais venu :)')
+            }else{
+                $("#result").css({'color':'orange'})
+                $("#result").html('Vous êtes déjà venu :(')
+            }
+        });
+
+    })
+
+    function getBrowserData() {
+        var result = serialize(navigator)
+        return result
+    }
+
+    function serialize(object) {
+        var type = typeof object;
+        if (object === null) {
+            return '"nullValue"';
+        }
+        if (type == 'string' || type === 'number' || type === 'boolean') {
+            return '"' + object + '"';
+        } else if (type === 'function') {
+            return '"functionValue"';
+        } else if (type === 'object') {
+            var output = '{';
+            for (var item in object) {
+                try {
+                    if (item !== 'enabledPlugin') {
+                        output += '"' + item + '":' + serialize(object[item]) + ',';
+                    }
+                } catch (e) {}
+            }
+            return output.replace(/\,$/, '') + '}';
+        } else if (type === 'undefined') {
+            return '"undefinedError"';
+        } else {
+            return '"unknownTypeError"';
+        }
+    }
+
+});
